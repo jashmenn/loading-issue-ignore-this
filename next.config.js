1b9e4a5f9641ccb1b3eadd3be11f43d96bbb68b5
Nate Murray <nate@natemurray.com>
2019-03-21 21:50:42 -0500

    basic babel loader doesn't work like i'd think

--- next.config.js
+++ next.config.js
@@ -1,8 +1,35 @@
 // see: https://github.com/zeit/next-plugins/tree/master/packages/next-typescript
 // next.config.js
-const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript({
-  webpack(config, options) {
-    return config
-  }
-})
+
+const webpack = (config, { dev, isServer }) => {
+  console.log("config: ", config);
+
+  config.module.rules.push({
+    test: /\.esm.js$/,
+    include: /node_modules\/@remirror/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              debug: true,
+              forceAllTransforms: true,
+              targets: {
+                ie: "11"
+              }
+            }
+          ]
+        ]
+      }
+    }
+  });
+
+  console.log("config.module.rules: ", config.module.rules);
+
+  return config;
+};
+
+const withTypescript = require("@zeit/next-typescript");
+module.exports = withTypescript({ webpack });
