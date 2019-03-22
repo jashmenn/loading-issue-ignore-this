1f94c5a4b328500d2f919d2bccf40056ce54d536
Nate Murray <nate@natemurray.com>
2019-03-21 21:36:08 -0500

    comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import { NodeExtension } from "@remirror/core"; // loads two different modules
 
 class TitlePlaceholder extends NodeExtension {}
 
-console.log("NodeExtension: ", NodeExtension);
-console.log("TitlePlaceholder: ", TitlePlaceholder);
+console.log("NodeExtension: ", NodeExtension); // is an ES6 `class` during SSR
+console.log("TitlePlaceholder: ", TitlePlaceholder); // is a function class
 
-const a = new TitlePlaceholder();
+// const a = new TitlePlaceholder();
+// gives => TypeError: Class constructor NodeExtension cannot be invoked without 'new'
 
 const Index = () => (
   <div>
