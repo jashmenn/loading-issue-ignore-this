f4d5dfee075633cbad80fcc34124562e21b41a50
Nate Murray <nate@natemurray.com>
2019-04-04 17:18:44 -0500

    cleanup next config

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -3,41 +3,8 @@
 const withTM = require("@weco/next-plugin-transpile-modules");
 
 const webpack = (config, { dev, isServer, defaultLoaders }) => {
-  /*
-  config.module.rules.push({
-    test: /\.js$/,
-    include: /node_modules\/some-es6-package/,
-    use: [
-      defaultLoaders.babel,
-      {
-        loader: "babel-loader",
-        options: {
-          babelrc: false,
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                debug: true,
-                forceAllTransforms: true,
-                targets: {
-                  ie: "11"
-                }
-              }
-            ]
-          ]
-        }
-      }
-    ]
-  });
-  */
-
   return config;
 };
 
 const withTypescript = require("@zeit/next-typescript");
-module.exports = withTypescript(
-  // withTM({
-  webpack
-  // transpileModules: ["some-es6-package", "remirror", "@remirror/core"]
-  // })
-);
+module.exports = withTypescript(webpack);
